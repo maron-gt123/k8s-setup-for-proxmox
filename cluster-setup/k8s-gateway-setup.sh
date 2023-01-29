956ea682ce4ba48d099ca00e9c7bc37db6500311
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2023-01-30 00:40:10 +0900

    Update k8s-gateway-setup.sh

--- cluster-setup/k8s-gateway-setup.sh
+++ cluster-setup/k8s-gateway-setup.sh
@@ -11,7 +11,6 @@ chmod 700 update.sh
 ./update.sh
 # endregion
 
-
 # region : setting ntp
 cat > /etc/systemd/timesyncd.conf <<EOF
 #  This file is part of systemd.
@@ -45,3 +44,50 @@ timedatectl set-timezone Asia/Tokyo
 timedatectl
 # endregion
 
+ssh_additional_config=$(cat <<EOF
+Host onp-k8s-cp-1
+   HostName 192.168.8.81
+   User cloudinit
+   IdentityFile /rsa/k8s_rsa
+
+ Host onp-k8s-cp-2
+   HostName 192.168.8.82
+   User cloudinit
+   IdentityFile /rsa/k8s_rsa
+
+ Host onp-k8s-cp-3
+   HostName 192.168.8.83
+   User cloudinit
+   IdentityFile /rsa/k8s_rsa
+
+ Host onp-k8s-wk-1
+   HostName 192.168.8.84
+   User cloudinit
+   IdentityFile /rsa/k8s_rsa
+
+ Host onp-k8s-wk-2
+   HostName 192.168.8.85
+   User cloudinit
+   IdentityFile /rsa/k8s_rsa
+
+ Host onp-k8s-wk-3
+   HostName 192.168.8.86
+   User cloudinit
+   IdentityFile /rsa/k8s_rsa
+   EOF
+)
+
+echo "${ssh_additional_config}" >> ~/.ssh/config
+
+cat > ssh-key.sh <<EOF
+#!/bin/bash
+ssh-keygen -R 192.168.8.81
+ssh-keygen -R 192.168.8.82
+ssh-keygen -R 192.168.8.83
+ssh-keygen -R 192.168.8.84
+ssh-keygen -R 192.168.8.85
+ssh-keygen -R 192.168.8.86
+EOF
+
+chmod 700 update.sh
+./ssh-key.sh

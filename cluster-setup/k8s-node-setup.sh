e29521b2dc2fd92aa56da3ea20eea1efe07eaea4
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2023-03-03 20:57:49 +0900

    Update k8s-node-setup.sh

--- cluster-setup/k8s-node-setup.sh
+++ cluster-setup/k8s-node-setup.sh
@@ -28,9 +28,9 @@ case $1 in
 esac
 
 # Set global variables
-KUBE_API_SERVER_VIP=192.168.18.100
+KUBE_API_SERVER_VIP=192.168.6.100
 VIP_INTERFACE=ens19
-NODE_IPS=( 192.168.18.81 192.168.18.82 192.168.18.83 )
+NODE_IPS=( 192.168.6.81 192.168.6.82 192.168.6.83 )
 
 # set per-node variables
 case $1 in

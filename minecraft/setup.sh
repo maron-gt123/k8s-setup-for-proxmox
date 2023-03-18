1a154fbfccca0dee8497f04c9605d3681426a23c
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2023-03-18 14:11:25 +0900

    Update setup.sh

--- minecraft/setup.sh
+++ minecraft/setup.sh
@@ -124,16 +124,16 @@ HOSTNAME=`hostname`
 # ---
 # region : stop minecraft
 # world messsage
-screen -p 0 -S ${SCREEN_NAME} -X eval 'stuff "say '${WAIT}'秒後にサーバーを停止し、バックアップ作業 に入ります\015"'
-screen -p 0 -S ${SCREEN_NAME} -X eval 'stuff "say 10分後に再接続可能になるので、しばらくお待ち下さい\015"'
+screen -p 0 -S "${SCREEN_NAME}" -X eval 'stuff "say '${WAIT}'秒後にサーバーを停止し、バックアップ作業 に入ります\015"'
+screen -p 0 -S "${SCREEN_NAME}" -X eval 'stuff "say 10分後に再接続可能になるので、しばらくお待ち下さい\015"'
 
 # minecraft stop cmd
-sleep $WAIT
-screen -p 0 -S ${SCREEN_NAME} -X eval 'stuff "stop\015"'
+sleep "$WAIT"
+screen -p 0 -S "${SCREEN_NAME}" -X eval 'stuff "stop\015"'
 # ---
 
 # minecraft restart
-$STARTSCRIPT
+"$STARTSCRIPT"
 # ---
 EOF
 

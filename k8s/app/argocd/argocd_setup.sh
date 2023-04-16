2fbf294356697667b3194aac2fb9d1a7a1af57d2
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2023-04-16 17:24:44 +0900

    Delete argocd_setup.sh

--- k8s/app/argocd/argocd_setup.sh
+++
@@ -1,7 +0,0 @@
-# region : create update.sh
-
-kubectl create namespace argocd
-sleep 10
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-sleep 10
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'

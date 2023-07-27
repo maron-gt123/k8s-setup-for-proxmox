376a7a8a32da3f04e6297f7b188976412f2d1149
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2023-07-27 13:59:21 +0900

    Update mic-start.sh

--- minecraft/script/paper/mic-start.sh
+++ minecraft/script/paper/mic-start.sh
@@ -3,11 +3,11 @@
 # region : set variables
 JARFILE=/minecraft/paper/paper-*
 MEM=2048M
+SCREEN_NAME=paper
 # endregion
-
-# ---
 
 # region : start minecraft
 cd `dirname $0`
-screen -AdmS paper java -server -Xms${MEM} -Xmx${MEM} -jar ${JARFILE} nogui
-# ---
+screen -AdmS ${SCREEN_NAME} java -server -Xms${MEM} -Xmx${MEM} -jar ${JARFILE} nogui
+
+# ---end---

e829f007d51b7a837b26b37b8e1ea6aedeff0132
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2023-07-27 13:51:26 +0900

    Update mic-start.sh

--- minecraft/script/lobby/mic-start.sh
+++ minecraft/script/lobby/mic-start.sh
@@ -3,11 +3,21 @@
 # region : set variables
 JARFILE=/minecraft/paper/paper-*
 MEM=2048M
+SCREEN_NAME=paper
 # endregion
 
 # ---
 
-# region : start minecraft
+# start minecraft
 cd `dirname $0`
-screen -AdmS paper java -server -Xms${MEM} -Xmx${MEM} -jar ${JARFILE} nogui
-# ---
+screen -AdmS ${SCREEN_NAME} java -server -Xms${MEM} -Xmx${MEM} -jar ${JARFILE} nogui
+
+# time and water cycle is false
+screen -p 0 -S ${SCREEN_NAME} -X eval 'stuff "gamerule doDaylightCycle false\015"'
+screen -p 0 -S ${SCREEN_NAME} -X eval 'stuff "gamerule doWeatherCycle false\015"'
+
+# time and water is set
+screen -p 0 -S ${SCREEN_NAME} -X eval 'stuff "time set day\015"'
+screen -p 0 -S ${SCREEN_NAME} -X eval 'stuff "weather clear\015"'
+
+# ---end---

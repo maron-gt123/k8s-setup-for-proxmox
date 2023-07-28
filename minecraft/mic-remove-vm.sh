705bd6a8af3aee908a33ffdb9ae02e421025abdf
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2023-07-28 20:41:57 +0900

    Update mic-remove-vm.sh

--- minecraft/mic-remove-vm.sh
+++ minecraft/mic-remove-vm.sh
@@ -4,12 +4,13 @@
 
 TARGET_BRANCH=$1
 TEMPLATE_VMID=9999
-vmid_lobby=201
-vmid_1=202
-vmid_2=203
-targetip_1=192.168.1.141
-targetip_2=192.168.1.142
-targetip_3=192.168.1.143
+VMID_LOBBY=201
+VMID_1=202
+VMID_2=203
+MARIA_DB=299
+TARGETIP_1=192.168.1.141
+TARGETIP_2=192.168.1.142
+TARGETIP_3=192.168.1.143
 
 # endregion
 
@@ -17,26 +18,28 @@ targetip_3=192.168.1.143
 # region : delete mic-VM
 
 # stop vm
-ssh "${targetip_1}" qm stop "${vmid_lobby}"
-ssh "${targetip_2}" qm stop "${vmid_1}"
-ssh "${targetip_3}" qm stop "${vmid_2}"
+ssh "${TARGETIP_1}" qm stop "${VMID_LOBBY}"
+ssh "${TARGETIP_2}" qm stop "${VMID_1}"
+ssh "${TARGETIP_3}" qm stop "${VMID_2}"
+ssh "${TARGETIP_2}" qm stop "${MARIA_DB}"
 
 # delete vm
 ## on onp-proxmox01-SV
-ssh "${targetip_1}" qm destroy "${vmid_lobby}" --destroy-unreferenced-disks true --purge true
-ssh "${targetip_1}" qm destroy "${TEMPLATE_VMID}" --destroy-unreferenced-disks true --purge true
+ssh "${TARGETIP_1}" qm destroy "${VMID_LOBBY}" --destroy-unreferenced-disks true --purge true
+ssh "${TARGETIP_1}" qm destroy "${TEMPLATE_VMID}" --destroy-unreferenced-disks true --purge true
 
 ## wait due to prevent to cluster-data mismatch on proxmox
 sleep 20s
 
 ## on onp-proxmox02-SV
-ssh "${targetip_2}" qm destroy "${vmid_1}" --destroy-unreferenced-disks true --purge true
+ssh "${TARGETIP_2}" qm destroy "${VMID_1}" --destroy-unreferenced-disks true --purge true
+ssh "${TARGETIP_2}" qm destroy "${VMID_DB}" --destroy-unreferenced-disks true --purge true
 
 ## wait due to prevent to cluster-data mismatch on proxmox
 sleep 20s
 
 ## on onp-proxmox03-SV
-ssh "${targetip_3}" qm destroy "${vmid_2}" --destroy-unreferenced-disks true --purge true
+ssh "${TARGETIP_3}" qm destroy "${VMID_2}" --destroy-unreferenced-disks true --purge true
 
 ## wait due to prevent to cluster-data mismatch on proxmox
 sleep 20s

bd701049370c214769abd40fb13bb348287b6ecd
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2023-08-04 02:48:26 +0900

    Update mic-deploy-vm.sh

--- minecraft/mic-deploy-vm.sh
+++ minecraft/mic-deploy-vm.sh
@@ -22,10 +22,10 @@ VM_LIST=(
     # targetip:   VMの配置先となるProxmoxホストのIP
     # targethost: VMの配置先となるProxmoxホストのホスト名
     # ---
-    #vmid   #vmname  #cpu  #mem     #vmsrvip     #targetip    #targethost
-    "201 mic-lobby-SV 4    4096  192.168.15.87 192.168.1.141 onp-prox01-SV"
-    "202 mic-paper-01 4    4096  192.168.15.88 192.168.1.142 onp-prox02-SV"
-    "203 mic-paper-02 4    4096  192.168.15.89 192.168.1.143 onp-prox03-SV"
+    #vmid   #vmname  #cpu  #mem     #vmsrvip     #vmsanip     #targetip    #targethost
+    "201 mic-lobby-SV 4    4096  192.168.15.87 192.168.1.87 192.168.1.141 onp-prox01-SV"
+    "202 mic-paper-01 4    4096  192.168.15.88 192.168.1.88 192.168.1.142 onp-prox02-SV"
+    "203 mic-paper-02 4    4096  192.168.15.89 192.168.1.89 192.168.1.143 onp-prox03-SV"
 )
 #endregion
 
@@ -39,7 +39,7 @@ wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.i
 # create a new VM and attach Network Adaptor
 # vmbr15=service Network Segment (192.168.15.0/24)
 # vmbr1=cluster Network Segment (192.168.1.0/24)
-qm create $TEMPLATE_VMID --cores 2 --memory 4096 --net0 virtio,bridge=vmbr15 --name minecraftSV-template
+qm create $TEMPLATE_VMID --cores 2 --memory 4096 --net0 virtio,bridge=vmbr15 --net1 virtio,bridge=vmbr1 --name minecraftSV-template
 
 # import the downloaded disk to $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME storage
 qm importdisk $TEMPLATE_VMID jammy-server-cloudimg-amd64.img $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME
@@ -69,7 +69,7 @@ rm jammy-server-cloudimg-amd64.img
 
 for array in "${VM_LIST[@]}"
 do
-    echo "${array}" | while read -r vmid vmname cpu mem vmsrvip targetip targethost
+    echo "${array}" | while read -r vmid vmname cpu mem vmsrvip vmsanip targetip targethost
     do
         # clone from template
         # in clone phase, can't create vm-disk to local volume
@@ -136,6 +136,12 @@ config:
       address: '${vmsrvip}'
       netmask: '255.255.255.0'
       gateway: '192.168.15.1'
+  - type: physical
+    name: ens19
+    subnets:
+    - type: static
+      address: '${vmsanip}'
+      netmask: '255.255.255.0'
   - type: nameserver
     address:
     - '9.9.9.9'

3ed746c461cea9caa8243d7d373ee0d42c4f5cd1
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2023-09-18 21:07:01 +0900

    Update k8s-node-setup.sh

--- k8s/k8s-node-setup.sh
+++ k8s/k8s-node-setup.sh
@@ -114,10 +114,10 @@ EOF
 sysctl --system
 
 # Install kubeadm
-curl -sLo /etc/apt/trusted.gpg.d/kubernetes-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
-apt-add-repository -y "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y kubelet=1.27.1-00 kubeadm=1.27.1-00 kubectl=1.27.1-00
+apt-get install -y kubelet=1.27.5-00 kubeadm=1.27.5-00 kubectl=1.27.5-00
 apt-mark hold kubelet kubeadm kubectl
 
 # Disable swap

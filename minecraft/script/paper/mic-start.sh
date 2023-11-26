a754aecef0a416531a3547b1a2270814c4385a08
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2023-11-26 10:41:49 +0900

    メモリ設定変更

--- minecraft/script/paper/mic-start.sh
+++ minecraft/script/paper/mic-start.sh
@@ -2,12 +2,13 @@
 
 # region : set variables
 JARFILE=/minecraft/paper/paper-*
-MEM=2048M
+MINMEM=1024M
+MAXMEM=2048M
 SCREEN_NAME=paper
 # endregion
 
 # region : start minecraft
 cd `dirname $0`
-screen -AdmS ${SCREEN_NAME} java -server -Xms${MEM} -Xmx${MEM} -jar ${JARFILE} nogui
+screen -AdmS ${SCREEN_NAME} java -server -Xms${MINMEM} -Xmx${MAXMEM} -jar ${JARFILE} nogui
 
 # ---end---

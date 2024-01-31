2434c785b5722c44e19a9961324fcd6ddb211582
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2024-01-31 23:17:51 +0900

    apt update付け忘れ

--- k8s/k8s-node-setup.sh
+++ k8s/k8s-node-setup.sh
@@ -115,8 +115,9 @@ sysctl --system
 # Install kubeadm
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get install -y kubeadm=1.28.5-1.1 kubectl=1.28.5-1.1 kubelet=1.28.5-1.1
-sudo apt-mark hold kubelet kubectl
+apt-get update
+apt-get install -y kubeadm=1.28.5-1.1 kubectl=1.28.5-1.1 kubelet=1.28.5-1.1
+apt-mark hold kubelet kubectl
 
 # Disable swap
 swapoff -a
@@ -190,7 +191,7 @@ EOF
 echo "net.ipv4.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
 sysctl -p
 
-apt-get -y install keepalived
+apt-get update && apt-get -y install keepalived
 
 cat > /etc/keepalived/keepalived.conf <<EOF
 # Define the script used to check if haproxy is still working

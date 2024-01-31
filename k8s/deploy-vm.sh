2ce022da112fda1da7c173b683fca779fba5e3da
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2024-01-31 19:02:28 +0900

    Update deploy-vm.sh

--- k8s/deploy-vm.sh
+++ k8s/deploy-vm.sh
@@ -9,7 +9,7 @@ TEMPLATE_BOOT_IMAGE_TARGET_VOLUME=iSCSI-network-01-lun01
 BOOT_IMAGE_TARGET_VOLUME=local-lvm
 SNIPPET_TARGET_VOLUME=prox-NFS
 SNIPPET_TARGET_PATH=/mnt/pve/${SNIPPET_TARGET_VOLUME}/snippets
-REPOSITORY_RAW_SOURCE_URL="https://raw.githubusercontent.com/maron-gt123/k8s-setup-for-proxmox/main"
+REPOSITORY_RAW_SOURCE_URL="https://raw.githubusercontent.com/maron-gt123/k8s-setup-for-proxmox/${TARGET_BRANCH}"
 
 VM_LIST=(
     # ---

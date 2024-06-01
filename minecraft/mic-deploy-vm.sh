e858694c616fd8f5f1eac7e409dc0732bb06a505
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2024-06-01 09:21:26 +0900

    vm-nic削除

--- minecraft/mic-deploy-vm.sh
+++ minecraft/mic-deploy-vm.sh
@@ -18,14 +18,13 @@ VM_LIST=(
     # cpu:        VMに割り当てるコア数(vCPU)
     # mem:        VMに割り当てるメモリ(MB)
     # vmsrvip:    VMのService Segment側NICに割り振る固定IP
-    # vmsanip:    VMのStorage Segment側NICに割り振る固定IP
     # targetip:   VMの配置先となるProxmoxホストのIP
     # targethost: VMの配置先となるProxmoxホストのホスト名
     # ---
-    #vmid   #vmname  #cpu  #mem     #vmsrvip     #vmsanip     #targetip    #targethost
-    "201 mic-lobby-SV 4    4096  192.168.15.87 192.168.1.87 192.168.1.141 onp-prox01-SV"
-    "202 mic-paper-01 4    4096  192.168.15.88 192.168.1.88 192.168.1.142 onp-prox02-SV"
-    "203 mic-paper-02 4    4096  192.168.15.89 192.168.1.89 192.168.1.143 onp-prox03-SV"
+    #vmid   #vmname  #cpu  #mem     #vmsrvip     #targetip    #targethost
+    "201 mic-lobby-SV 4    4096  192.168.15.87 192.168.1.141 onp-prox01-SV"
+    "202 mic-paper-01 4    4096  192.168.15.88 192.168.1.142 onp-prox02-SV"
+    "203 mic-paper-02 4    4096  192.168.15.89 192.168.1.143 onp-prox03-SV"
 )
 #endregion
 
@@ -35,55 +34,39 @@ VM_LIST=(
 
 # download the image(ubuntu 22.04 LTS)
 wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
-
 # create a new VM and attach Network Adaptor
 # vmbr15=service Network Segment (192.168.15.0/24)
-# vmbr1=cluster Network Segment (192.168.1.0/24)
-qm create $TEMPLATE_VMID --cores 2 --memory 4096 --net0 virtio,bridge=vmbr15 --net1 virtio,bridge=vmbr1 --name minecraftSV-template
-
+qm create $TEMPLATE_VMID --cores 2 --memory 4096 --net0 virtio,bridge=vmbr15 --name minecraftSV-template
 # import the downloaded disk to $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME storage
 qm importdisk $TEMPLATE_VMID jammy-server-cloudimg-amd64.img $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME
-
 # finally attach the new disk to the VM as scsi drive
 qm set $TEMPLATE_VMID --scsihw virtio-scsi-pci --scsi0 $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME:vm-$TEMPLATE_VMID-disk-0
-
 # add Cloud-Init CD-ROM drive
 qm set $TEMPLATE_VMID --ide2 $CLOUDINIT_IMAGE_TARGET_VOLUME:cloudinit
-
 # set the bootdisk parameter to scsi0
 qm set $TEMPLATE_VMID --boot c --bootdisk scsi0
-
 # set serial console
 qm set $TEMPLATE_VMID --serial0 socket --vga serial0
-
 # migrate to template
 qm template $TEMPLATE_VMID
-
 # cleanup
 rm jammy-server-cloudimg-amd64.img
-
 #endregion
-# ---
 
 # region create vm from template
-
 for array in "${VM_LIST[@]}"
 do
     echo "${array}" | while read -r vmid vmname cpu mem vmsrvip vmsanip targetip targethost
     do
         # clone from template
         # in clone phase, can't create vm-disk to local volume
-        qm clone "${TEMPLATE_VMID}" "${vmid}" --name "${vmname}" --full true --target "${targethost}"
-        
+        qm clone "${TEMPLATE_VMID}" "${vmid}" --name "${vmname}" --full true --target "${targethost}"        
         # set compute resources
         ssh -n "${targetip}" qm set "${vmid}" --cores "${cpu}" --memory "${mem}"
-
         # move vm-disk to local
         ssh -n "${targetip}" qm move-disk "${vmid}" scsi0 "${BOOT_IMAGE_TARGET_VOLUME}" --delete true
-
         # resize disk (Resize after cloning, because it takes time to clone a large disk)
         ssh -n "${targetip}" qm resize "${vmid}" scsi0 30G
-
         # create snippet for cloud-init(user-config)
         # START irregular indent because heredoc
 # ----- #
@@ -118,6 +101,8 @@ runcmd:
   - su - cloudinit -c "sudo apt install -y zip"
   - su - cloudinit -c "sudo apt install -y git"
   - su - cloudinit -c "sudo bash ~/minecraft-setup.sh"
+  # change default shell to bash
+  - chsh -s $(which bash) cloudinit
 EOF
 
 # ----- #
@@ -136,12 +121,6 @@ config:
       address: '${vmsrvip}'
       netmask: '255.255.255.0'
       gateway: '192.168.15.1'
-  - type: physical
-    name: ens19
-    subnets:
-    - type: static
-      address: '${vmsanip}'
-      netmask: '255.255.255.0'
   - type: nameserver
     address:
     - '192.168.10.132'

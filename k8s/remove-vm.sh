b4ec45ca0e1583d8d7942d7fefb2162672cd3ad9
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2024-07-07 08:37:30 +0900

    Update remove-vm.sh

--- k8s/remove-vm.sh
+++ k8s/remove-vm.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # region : set variables
-
 TARGET_BRANCH=$1
 TEMPLATE_VMID=8888
 CP01=801
@@ -13,11 +12,7 @@ WK03=883
 TARGETIP_1=192.168.1.141
 TARGETIP_2=192.168.1.142
 TARGETIP_3=192.168.1.143
-
 # endregion
-
-
-# region : delete mic-VM
 
 # stop vm
 ssh "${TARGETIP_1}" qm stop "${CP01}"
@@ -27,28 +22,20 @@ ssh "${TARGETIP_1}" qm stop "${WK01}"
 ssh "${TARGETIP_2}" qm stop "${WK02}"
 ssh "${TARGETIP_3}" qm stop "${WK03}"
 
-# delete vm
-## on onp-proxmox01-SV
+# delete vm on onp-proxmox01-SV
 ssh "${TARGETIP_1}" qm destroy "${CP01}" --destroy-unreferenced-disks true --purge true
 ssh "${TARGETIP_1}" qm destroy "${WK01}" --destroy-unreferenced-disks true --purge true
 ssh "${TARGETIP_1}" qm destroy "${TEMPLATE_VMID}" --destroy-unreferenced-disks true --purge true
-
-## wait due to prevent to cluster-data mismatch on proxmox
 sleep 20s
 
-## on onp-proxmox02-SV
+# delete vm on onp-proxmox02-SV
 ssh "${TARGETIP_2}" qm destroy "${CP02}" --destroy-unreferenced-disks true --purge true
 ssh "${TARGETIP_2}" qm destroy "${WK02}" --destroy-unreferenced-disks true --purge true
-
-## wait due to prevent to cluster-data mismatch on proxmox
 sleep 20s
 
-## on onp-proxmox03-SV
+# delete vm on onp-proxmox03-SV
 ssh "${TARGETIP_3}" qm destroy "${CP03}" --destroy-unreferenced-disks true --purge true
 ssh "${TARGETIP_3}" qm destroy "${WK03}" --destroy-unreferenced-disks true --purge true
-
-## wait due to prevent to cluster-data mismatch on proxmox
 sleep 20s
 
-
-# endregion
+echo "-----end-----"

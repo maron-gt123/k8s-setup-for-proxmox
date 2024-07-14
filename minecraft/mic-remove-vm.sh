38939a77b78abba43f60e019014b946d8c0aa0a1
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2024-07-14 22:19:52 +0900

    Update mic-remove-vm.sh

--- minecraft/mic-remove-vm.sh
+++ minecraft/mic-remove-vm.sh
@@ -3,33 +3,25 @@
 # region : set variables
 TARGET_BRANCH=$1
 TEMPLATE_VMID=9999
-VMID_LOBBY=201
-VMID_1=202
-VMID_2=203
+VMID_1=201
+VMID_2=202
 TARGETIP_1=192.168.1.141
 TARGETIP_2=192.168.1.142
-TARGETIP_3=192.168.1.143
 # endregion
 
 
 # stop vm
-ssh "${TARGETIP_1}" qm stop "${VMID_LOBBY}"
 ssh "${TARGETIP_2}" qm stop "${VMID_1}"
 ssh "${TARGETIP_3}" qm stop "${VMID_2}"
 
 # delete vm
 ## on onp-proxmox01-SV
-ssh "${TARGETIP_1}" qm destroy "${VMID_LOBBY}" --destroy-unreferenced-disks true --purge true
+ssh "${TARGETIP_1}" qm destroy "${VMID_1}" --destroy-unreferenced-disks true --purge true
 ssh "${TARGETIP_1}" qm destroy "${TEMPLATE_VMID}" --destroy-unreferenced-disks true --purge true
 ## wait due to prevent to cluster-data mismatch on proxmox
 sleep 20s
 
 ## on onp-proxmox02-SV
-ssh "${TARGETIP_2}" qm destroy "${VMID_1}" --destroy-unreferenced-disks true --purge true
-## wait due to prevent to cluster-data mismatch on proxmox
-sleep 20s
-
-## on onp-proxmox03-SV
-ssh "${TARGETIP_3}" qm destroy "${VMID_2}" --destroy-unreferenced-disks true --purge true
+ssh "${TARGETIP_2}" qm destroy "${VMID_2}" --destroy-unreferenced-disks true --purge true
 ## wait due to prevent to cluster-data mismatch on proxmox
 sleep 20s

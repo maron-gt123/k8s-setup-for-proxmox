1ae1e165b780b8cf552581a0f8b74d3ac59809d0
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2024-12-31 21:39:17 +0900

    Update k8s-node-setup.sh

--- k8s/k8s-node-setup.sh
+++ k8s/k8s-node-setup.sh
@@ -79,8 +79,21 @@ EOF
 # Apply sysctl params without reboot
 sudo sysctl --system
 
-## Install containerd
-sudo apt-get update && sudo apt-get install -y containerd apt-transport-https curl gnupg2
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y containerd.io
 
 # Configure containerd
 sudo mkdir -p /etc/containerd

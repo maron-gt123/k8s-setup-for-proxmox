ed01d363cff0df41354ede07bef735ca8d92e7e3
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2025-04-28 05:07:34 +0900

    ver 1.32.1-1.1

--- k8s/k8s-node-setup.sh
+++ k8s/k8s-node-setup.sh
@@ -129,7 +129,7 @@ sysctl --system
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubeadm=1.32.0-1.1 kubectl=1.32.0-1.1 kubelet=1.32.0-1.1
+sudo apt-get install -y kubeadm=1.32.1-1.1 kubectl=1.32.1-1.1 kubelet=1.32.1-1.1
 sudo apt-mark hold kubelet kubectl
 
 # Disable swap
